c9690e56b61f0d112aea780e8e685631777534e1
nazanin <nsarrafzade@yahoo.com>
2019-03-28 12:09:28 +0430

    all added

---
+++ routes/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('index', { title: 'Form Validation', success: req.session.success, errors: req.session.errors });
+    req.session.errors = null;
+    req.session.success = null;
+});
+
+router.post('/submit', function(req, res, next) {
+    req.check('email', 'Invalid email').isEmail();
+    req.check('password', 'Password is invalid').isLength({min: 3}).equals(req.body.conf);
+
+    var errors = req.validationErrors();
+    if (errors) {
+        req.session.errors = errors;
+        req.session.success = false;
+    } else {
+        req.session.success = true;
+    }
+    res.redirect('/');
+});
+
+module.exports = router;
